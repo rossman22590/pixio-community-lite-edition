d1b1ad0c871b79b05204860e683c0b61238380d7
Ross Cohen <rosscohen1990@gmail.com>
2026-06-09 01:59:50 -0400

    update

--- pages/api/postToDiscord.js
+++ pages/api/postToDiscord.js
@@ -1,22 +1,23 @@
-import { Client, Intents, MessageAttachment } from 'discord.js';
+import { AttachmentBuilder, Client, GatewayIntentBits } from 'discord.js';
 
 export default async function handler(req, res) {
   const { message, imageUrl } = req.body;
-  
+
   console.log('Received image url:', imageUrl);
 
-  const intents = new Intents([Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]);
-  const client = new Client({ intents });
+  const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  });
 
   try {
     await client.login(process.env.BOT_TOKEN);
 
     const channel = await client.channels.fetch(process.env.CHANNEL_ID);
-    if (!channel.isText()) {
+    if (!channel?.isTextBased()) {
       throw new Error('Channel must be a text-based channel');
     }
 
-    const attachment = new MessageAttachment(imageUrl, 'image.png');
+    const attachment = new AttachmentBuilder(imageUrl, { name: 'image.png' });
     await channel.send({ content: message, files: [attachment] });
 
     res.status(200).json({ status: 'success' });
